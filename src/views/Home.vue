9400537b245eb05a6b727dc77a90f1a9694a6eeb
devmount <mail@devmount.de>
2019-06-21 16:10:26 +0200

    :heavy_plus_sign: add json test data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,23 @@
 div.home
   img(alt='Vue logo' src='../assets/logo.png')
   HelloWorld(msg='Welcome to Your Vue.js App').
+  | {{ test }}
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Videos from '@/data/videos.json'
 
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      test: Videos.test
+    }
   }
 }
 </script>
