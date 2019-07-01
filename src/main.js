bdbb35e55df5b0e8b6ec909c69ba479200598184
devmount <mail@devmount.de>
2019-07-01 17:38:16 +0200

    :gear: handle released series with non-released episodes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
+/* eslint-disable */
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+// extend Object for filtering
+Object.filter = (obj, predicate) =>
+  Object.keys(obj)
+    .filter(key => predicate(obj[key]))
+    .reduce((res, key) => (res[key] = obj[key], res), {})
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template lang="pug">
 div.container.home
   //- show selection
-  div.feature(v-if='active.episode !== false' :style='seriesReleased(active.series) ? "background-image: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)), url(http://i3.ytimg.com/vi/" + database[active.series].episodes[active.episode].youtube + "/maxresdefault.jpg)" : "background: transparent"')
+  div.feature(v-if='active.episode !== false' :style='episodeReleased(active.series, active.episode) ? "background-image: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)), url(http://i3.ytimg.com/vi/" + database[active.series].episodes[active.episode].youtube + "/maxresdefault.jpg)" : "background: transparent"')
     div.thumb(v-if='!active.play')
-      img.c-hand(v-if='seriesReleased(active.series)' :src="'http://i3.ytimg.com/vi/' + database[active.series].episodes[active.episode].youtube + '/maxresdefault.jpg'" @click='active.play = true')
+      img.c-hand(v-if='episodeReleased(active.series, active.episode)' :src="'http://i3.ytimg.com/vi/' + database[active.series].episodes[active.episode].youtube + '/maxresdefault.jpg'" @click='active.play = true')
       img(v-else src="@/assets/coming-soon-640x360.png")
     iframe(v-else width='640' height='360' :src="'https://www.youtube.com/embed/' + database[active.series].episodes[active.episode].youtube" frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen).
     div.description
-      span(v-if='seriesReleased(active.series)') {{ database[active.series].episodes[active.episode].title }}
-      span(v-else) Neuer Titel verfügbar am {{ tstampToDate(database[active.series].episodes[1].release) }}
+      span(v-if='seriesReleased(active.series)')
+        span(v-if='episodeReleased(active.series, active.episode)') {{ database[active.series].episodes[active.episode].title }}
+        span(v-else) Episode verfügbar am {{ tstampToDate(database[active.series].episodes[active.episode].release) }}
+      span(v-else) Neuer Titel verfügbar am {{ tstampToDate(database[active.series].episodes[active.episode].release) }}
       span.tag.ml-2(v-if='seriesReleased(active.series) && database[active.series].episodes[active.episode].hasOwnProperty("tags")' v-for='(tag, t) in database[active.series].episodes[active.episode].tags' :key='t')
         | {{ tag }}
   //- show default feature
@@ -19,19 +21,20 @@ div.container.home
       | Wähle einen Titel
       i.icon.ion-md-arrow-down.ml-1
   //- show list of all series
-  h2 {{ Object.keys(database).length }} Titel
+  h2 {{ seriesCount }} Titel
   hooper.series(:settings='sliderSettings.series' ref='series')
     slide(v-for='(series, s, i) in database' :key='s' :index='i' :class='{ active: active.series == s }')
       img(v-if='seriesReleased(s)' :src="'http://i3.ytimg.com/vi/' + series.episodes[1].youtube + '/mqdefault.jpg'" @click='active.series = s; active.episode = 1; active.play = false')
       img(v-else src="@/assets/unknown-320x180.jpg" @click='active.series = s; active.episode = 1; active.play = false')
     hooper-navigation(slot='hooper-addons')
   //- show episode list of current series
-  h2(v-if='active.series !== false && Object.keys(database[active.series].episodes).length > 1 && seriesReleased(active.series)')
-    | {{ Object.keys(database[active.series].episodes).length }} Episoden in {{ database[active.series].title }}
+  h2(v-if='active.series !== false && seriesReleased(active.series)')
+    span {{ releasedEpisodeCount }} Episode{{ releasedEpisodeCount == 1 ? '' : 'n' }} in {{ database[active.series].title }}
     span.text-unimportant.ml-1 {{ database[active.series].subtitle }}
-  hooper.episodes(v-if='active.series == s && Object.keys(series.episodes).length > 1 && seriesReleased(active.series)' v-for='(series, s) in database' :key='s' :settings='sliderSettings.episodes' :ref='s')
+  hooper.episodes(v-if='active.series == s && episodeCount > 1 && seriesReleased(active.series)' v-for='(series, s) in database' :key='s' :settings='sliderSettings.episodes' :ref='s')
     slide(v-for='(episode, e, i) in series.episodes' :key='i' :index='i' :class='{ active: active.episode == e }')
-      img(:src="'http://i3.ytimg.com/vi/' + episode.youtube + '/mqdefault.jpg'" @click='active.episode = e; active.play = false')
+      img(v-if='episodeReleased(s, e)' :src="'http://i3.ytimg.com/vi/' + episode.youtube + '/mqdefault.jpg'" @click='active.episode = e; active.play = false')
+      img(v-else src="@/assets/unknown-320x180.jpg" @click='active.series = s; active.episode = e; active.play = false')
     hooper-navigation(slot='hooper-addons')
   //- div(v-if='active.series !== false' class='text-unimportant text-small mt-2')
   //-   span(v-for='line in database[active.series].description.split("\\n")')
@@ -101,12 +104,26 @@ export default {
   },
   methods: {
     seriesReleased (skey) {
-      return Number(this.now) > Number(this.database[skey].episodes[1].release)
+      return this.episodeReleased(skey, 1)
+    },
+    episodeReleased (skey, ekey) {
+      return Number(this.now) > Number(this.database[skey].episodes[ekey].release)
     },
     tstampToDate (t) {
       var event = new Date(Number(t + '000'))
       var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
       return event.toLocaleDateString('de-DE', options) + ' um ' + event.toLocaleTimeString('de-DE').slice(0, 5) + ' Uhr'
+    }
+  },
+  computed: {
+    seriesCount () {
+      return Object.keys(this.database).length
+    },
+    episodeCount () {
+      return Object.keys(this.database[this.active.series].episodes).length
+    },
+    releasedEpisodeCount () {
+      return Object.keys(Object.filter(this.database[this.active.series].episodes, e => Number(this.now) > Number(e.release))).length
     }
   }
 }
