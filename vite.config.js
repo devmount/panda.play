7186954d91924bf0fb2918c6b31be0fcd607a063
Andreas Müller <mail@devmount.de>
2025-08-01 22:46:27 +0200

    📦 Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
-import oxlintPlugin from 'vite-plugin-oxlint'
 
 export default defineConfig({
-	plugins: [vue(), oxlintPlugin()],
+	plugins: [vue()],
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
 	},
